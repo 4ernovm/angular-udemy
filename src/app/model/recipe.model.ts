20f686c862a4e50e6a26484d5582278127155dbf
chernoff <4ernovm@gmail.com>
2017-06-28 17:18:09 +0300

    Scaffold

---
+++ src/app/model/recipe.model.ts
@@ -0,0 +1,22 @@
+import {Ingredient} from "./ingredient.model";
+
+export class Recipe
+{
+    name: string;
+    description: string;
+    ingredients: [Ingredient];
+
+    constructor(name: string, description: string, ingredients: [Ingredient])
+    {
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+    }
+
+    addIngredient(ingredient: Ingredient): Recipe
+    {
+        this.ingredients.push(ingredient);
+
+        return this;
+    }
+}
